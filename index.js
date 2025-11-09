144c48a1ea4817e95e171e0cdab21b04720b7df4
OpuTheCoder <artistop26@gmail.com>
2025-11-10 00:54:56 +0600

    basic crud courses added

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 3000
+
+// middleware
+app.use(cors());
+app.use(express.json())
+
+const uri = "mongodb+srv://skillPathUser:U0zzlsNWXbVU0zAP@cluster0.q4baesu.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('SkillPath Server is running')
+})
+
+async function run() {
+    try {
+        await client.connect();
+
+        const db = client.db('skill_db');
+        const coursesCollection = db.collection('courses');
+
+        app.get('/courses', async(req, res) => {
+            const cursor = coursesCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/courses/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id)}
+            const result = await coursesCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.post('/courses', async (req, res) => {
+            const newCourse = req.body;
+            const result = await coursesCollection.insertOne(newCourse);
+            res.send(result);
+        })
+
+        app.patch('/courses/:id', async(req, res) => {
+            const id = req.params.id;
+            const updatedCourse = req.body;
+            const query = { _id: new ObjectId(id) }
+            const update = {
+                $set: {
+                    name: updatedCourse.name,
+                    price: updatedCourse.price
+                }
+            }
+            const result = await coursesCollection.updateOne(query, update)
+            res.send(result)
+        })
+
+        app.delete('/courses/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await coursesCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
+
+app.listen(port, () => {
+    console.log(`SkillPath Server is running on port ${port}`)
+})
